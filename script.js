453a0e499d1227be05a8920aa38c9ad639e2c5c7
AlexanderBeichuk <alexander.beichuk@gmail.com>
2018-07-11 11:51:55 +0300

    Init

---
+++ script.js
@@ -0,0 +1,41 @@
+/**
+ * Created by user on 11.07.2018.
+ */
+
+var AWS = require('aws-sdk');
+var s3Zip = require('s3-zip');
+
+exports.handler = function (event, context) {
+  console.log('event', event);
+
+
+  var region = 'REGION';
+  var bucket = 'BUCKET';
+  var folder = 'FOLDER';
+  var files = 'FILES';
+  var zipFileName = 'FILENAME';
+
+  try {
+
+    var body = s3Zip.archive({ region: region, bucket: bucket}, folder, files);
+    var zipParams = { params: { Bucket: bucket, Key: folder + zipFileName } };
+    var zipFile = new AWS.S3(zipParams);
+    zipFile.upload({ Body: body })
+      .on('httpUploadProgress', function (evt) { console.log(evt) })
+      .send(function (e, r) {
+        if (e) {
+          var err = 'zipFile.upload error ' + e;
+          console.log(err);
+          context.fail(err)
+        }
+        console.log(r);
+        context.succeed(r)
+      })
+
+  } catch (e) {
+    var err = 'catched error: ' + e;
+    console.log(err);
+    context.fail(err)
+  }
+
+};
