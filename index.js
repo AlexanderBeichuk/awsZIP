a5120ab0ffee6e2b3fb4c15dc4ae348c312ec491
AlexanderBeichuk <alexander.beichuk@gmail.com>
2018-07-18 20:37:52 +0300

    new version

--- index.js
+++ index.js
@@ -1,59 +1,44 @@
 var AWS = require('aws-sdk');
 var s3Zip = require('s3-zip');
+var fs = require('fs');
+var join = require('path').join;
 
 exports.handler = function (event, context) {
-  console.log('event', event);
+    console.log('event', event);
 
-  const regex = /(?:\/\/s3\.amazonaws.com\/([^\/]+)|:\/\/([^.]+)\.s3\.amazonaws\.com)\/([^\/]+)/;
-  const urlComponents = event.data[0].split(regex);
-  const separateIndex = urlComponents[4].lastIndexOf('/');
+    const regex = /(?:\/\/s3\.amazonaws.com\/([^\/]+)|:\/\/([^.]+)\.s3\.amazonaws\.com)\/([^\/]+)/;
+    const urlComponents = event.data[0].split(regex);
+    const separateIndex = urlComponents[4].lastIndexOf('/');
 
-  const region = 'us-west-1';
-  const bucket = urlComponents[2];
-  const folder = urlComponents[3] + urlComponents[4].slice(0, separateIndex);
-  const zipFileName = 'partnumber-rev.zip';
-  let files = [];
+    const region = 'us-west-1';
+    const bucket = urlComponents[2];
+    const folder = urlComponents[3] + urlComponents[4].slice(0, separateIndex) + '/';
+    const zipFileName = 'partnumber-rev.zip';
+    let files = [];
 
 
+    event.data.forEach(item => {
+        const urlName = item.split('/');
+        files.push(urlName[urlName.length - 1]);
+    });
 
-  event.data.forEach(item => {
-    const urlName = item.split('/');
-  files.push(urlName[urlName.length-1]);
-});
+    console.log('------------------------------------------------------');
+    console.log(bucket);
+    console.log(folder);
+    console.log(files);
+    console.log('------------------------------------------------------');
 
-  console.log('------------------------------------------------------');
-  console.log(bucket);
-  console.log(folder);
-  console.log(files);
-  console.log('------------------------------------------------------');
-
-  try {
-
-    const body = s3Zip.archive({ region: region, bucket: bucket}, folder, files);
     console.log('**********1***********');
-    const zipParams = { params: { Bucket: bucket, Key: folder + zipFileName } };
-    const zipFile = new AWS.S3(zipParams);
-    console.log('**********2***********');
+    const output = fs.createWriteStream(join(__dirname, zipFileName));
 
-    zipFile.upload({ Body: body })
-      .on('httpUploadProgress', uploadEvent => {
-      console.log('**********3***********');
-      console.log(uploadEvent);
-  })
-  .send((uploadError, uploadResponse) => {
-      console.log('**********4***********');
-    if (uploadError) {
-      context.fail(uploadError);
-      callback(null, zipFile);
-    }
-    context.succeed(uploadResponse);
-    callback(null, zipFile);
-  });
-
-  } catch (zipError) {
-    console.log('**********5***********');
-    context.fail(zipError);
-    callback(null, zipError);
-  }
-  console.log('**********6***********');
-};
+    s3Zip
+        .archive({region: region, bucket: bucket}, folder, files)
+        .pipe(output)
+        .pipe(function () {
+            console.log('**********2***********');
+            callback(null, console.log(output));
+            console.log('**********2***********');
+        });
+    //callback(null, body);
+    console.log('**********END***********');
+}
