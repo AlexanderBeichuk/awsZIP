6aa0056cebe0f049db2fc5eb6560dff31ed0d8d8
AlexanderBeichuk <alexander.beichuk@gmail.com>
2018-07-27 18:07:38 +0300

    Upload to another bucket

--- index.js
+++ index.js
@@ -5,41 +5,46 @@ const join = require('path').join;
 
 exports.handler = function (event, context, callback) {
 
-  /*const regex = /(?:\/\/s3\.amazonaws.com\/([^\/]+)|:\/\/([^.]+)\.s3\.amazonaws\.com)\/([^\/]+)/;
+  const regex = /(?:\/\/s3\.amazonaws.com\/([^\/]+)|:\/\/([^.]+)\.s3\.amazonaws\.com)\/([^\/]+)/;
    const urlComponents = event.data[0].split(regex);
-   const separateIndex = urlComponents[4].lastIndexOf('/');*/
+   const separateIndex = urlComponents[4].lastIndexOf('/');
 
   const region = 'us-west-1';
-  const bucket = /*urlComponents[2]*/ 'testzipbucket';
-  const folder = /*urlComponents[3] + urlComponents[4].slice(0, separateIndex) +*/ 'questions' + '/';
-  const zipFileName = 'partnumber-rev.zip';
+  const bucket = urlComponents[2] /*'testzipbucket'*/;
+  const folder = urlComponents[3] + urlComponents[4].slice(0, separateIndex) + '/';
+  const folderSplit = folder.split('/');
+  /*const key = folder + zipFileName;*/
   let files = [];
 
   event.data.forEach(item => {
     const urlName = item.split('/');
     files.push(urlName[urlName.length - 1]);
   });
-  /*const output = fs.createWriteStream(join('/tmp', zipFileName));
-   s3Zip
-   .archive({region: region, bucket: bucket}, folder, files)
-   .pipe(output);
-   output
-   .on('finish', () => {
-   fs.readFile('/tmp/' + zipFileName, 'utf8', (readFileError, contents) => {
-   if (readFileError) {
-   callback(readFileError);
-   }
-   callback(null, contents);
-   })
-   })*/
 
   try {
 
     const body = s3Zip.archive({ region: region, bucket: bucket}, folder, files);
-    const zipParams = {params: {Bucket: bucket, Key: folder + zipFileName}};
+
+    const zip = {
+      bucket: 'fuseplm-attachments',
+      folder: 'zip/',
+      fileName: folderSplit[folderSplit.length - 2]/*'partnumber-rev-' + Math.floor(Math.random() * Math.floor(10000000))*/ + '.zip'
+    };
+
+    const zipParams = {
+      params: {
+        Bucket: zip.bucket,
+        Key: zip.folder + zip.fileName,
+        ACL: 'public-read'
+      }
+    };
+
     const zipFile = new AWS.S3(zipParams);
 
-    zipFile.upload({Body: body})
+    zipFile
+      .upload({
+        Body: body
+      })
       .on('httpUploadProgress', function (uploadProgress) {
         console.log('uploadProgress ', uploadProgress);
       })
@@ -49,64 +54,36 @@ exports.handler = function (event, context, callback) {
           callback(uploadError)
         }
         console.log(uploadSuccess);
-        const params = {Bucket: bucket, Key: 'questions/partnumber-rev.zip'};
 
-        new AWS.S3().getObject(params, (errorFile, data) => {
+        /*const params = {
+          Bucket: 'testzipbucket',
+          Key: 'questions/' + zipFileName,
+          ACL: 'public-read'
+        };*/
+
+        const responseURL = 'https://s3-us-west-1.amazonaws.com/' + zip.bucket + '/' + zip.folder + zip.fileName;
+        callback(null, responseURL);
+
+        /*zipFile.putObject(params, (errorFile, data) => {
           if (errorFile) {
             console.log(errorFile);
             callback(errorFile);
           }
+          console.log('****************DATA***********');
           console.log(data);
 
-          new AWS.S3().deleteObject(params, (errorDelete, successDelete) => {
+          /!*new AWS.S3().deleteObject(params, (errorDelete, successDelete) => {
             if (errorDelete) {
               console.log(errorDelete);
               callback(errorDelete);
             }
             console.log(successDelete);
             callback(null, data);
-          });
-        });
+          });*!/
+        });*/
       })
   } catch (catchedError) {
     console.log('Catched error: ' + catchedError);
     callback(catchedError);
   }
 };
-
-  /*const output = fs.createWriteStream(join('/tmp', zipFileName));
-   s3Zip
-   .archive({region: region, bucket: bucket}, folder, files)
-   .pipe(output);
-
-   output
-   .on('finish', (a,b,c) => {
-   /!*console.log('************LOGS*************');
-   console.log(a);
-   console.log(b);
-   console.log(c);*!/
-   output.close((d,e,f) => {
-   /!*console.log(d);
-   console.log(e);
-   console.log(f);
-   console.log('************LOGS*************');*!/
-   fs.readFile('/tmp/' + zipFileName, 'utf8', (readFileError, contents) => {
-   if (readFileError) {
-   callback(readFileError);
-   }
-   const params = {Bucket: 'testzipbucket', Key: zipFileName, Body: contents };
-   new AWS.S3().putObject(params, function(uploadError, data) {
-   if (uploadError) {
-   console.log('**************************UPLOAD ERROR*************************');
-   console.log(uploadError);
-   callback(uploadError);
-   }
-   console.log("Successfully uploaded data to myBucket/myKey");
-   callback(null, contents);
-   });
-   });
-   })
-   })
-   .on('error', (outputError) => {
-   callback(outputError);
-   });*/
